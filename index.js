af18dcf138d04b6dda9b161859db60397f50ec58
Basquiat <jlnmcneal@gmail.com>
2022-04-07 03:42:24 -0500

    Inquirer Questions

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generate = require('generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt([
+    {
+        type: "input",
+        message: 'README Heading',
+        name: 'heading',
+    },
+    {
+        type: 'input',
+        message: 'Project description',
+        name: 'description'
+    },
+    {
+        type: "input",
+        message: 'Does your project require installing dependicies?',
+        name: 'installation',
+    },
+    {
+        type: "input",
+        message: 'How to use it?',
+        name: 'usage',
+    },
+    {
+        type:'input',
+        message: 'Your Full Name?',
+        name: 'author',
+        default: 'Jalen Mcneal',
+    },
+    {
+        type:'input',
+        message: 'Your Email',
+        name: 'email',
+        default: 'mcneal.jalen@yahoo.com'
+    },
+    {
+        type: 'input',
+        message: 'Github account',
+        name: 'github',
+        default:'Ja1993en',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: "input",
+        message: 'Any guidlines for contributing to your project?',
+        name: 'Contributors',
+    },
+    {
+        type: "input",
+        message: 'What tests will you be using',
+        name: 'testingInfo'
+    },
+    {
+        type: 'list',
+        message: 'Lience',
+        name:'license',
+        choices: ['MIT', 'GPLv3', 'GPL'],
+    },
+])
+    .then((data) => {
+        fs.writeFile('README.md', generateMarkdown(data), (err) => err ? console.log(err) : co
+        );
+    })
 
-// Function call to initialize app
-init();
+
+    // {
+    //     type: "list",
+    //     message: ''
+    //     name: ''
+    // },
